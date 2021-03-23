311f05bfd6b06ea36234d505651ed1ad1d59fe9f
Hyunsu Bang <hyunsu.bang@jetsynthesys.com>
2021-03-24 00:32:52 +0900

    Assignment

---
+++ bird13/states/ScoreState.lua
@@ -0,0 +1,48 @@
+--[[
+    ScoreState Class
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    A simple state used to display the player's score before they
+    transition back into the play state. Transitioned to from the
+    PlayState when they collide with a Pipe.
+]]
+
+ScoreState = Class{__includes = BaseState}
+
+--[[
+    When we enter the score state, we expect to receive the score
+    from the play state so we know what to render to the State.
+]]
+function ScoreState:enter(params)
+    self.score = params.score
+    self.medal = love.graphics.newImage('medal.png');
+    self.x = VIRTUAL_WIDTH / 2 - 22
+    self.y = VIRTUAL_HEIGHT / 2 - 36
+
+end
+
+function ScoreState:update(dt)
+    -- go back to play if enter is pressed
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function ScoreState:render()
+    -- simply render the score to the middle of the screen
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+
+    local adjust = 0;
+    if (self.score >= 10) then
+        love.graphics.draw(self.medal, self.x, self.y)
+        love.graphics.printf('You won the medal!', 0, 180 + adjust, VIRTUAL_WIDTH, 'center')
+        adjust = 60;
+    end
+
+    love.graphics.printf('Press Enter to Play Again!', 0, 160 + adjust, VIRTUAL_WIDTH, 'center')
+end
